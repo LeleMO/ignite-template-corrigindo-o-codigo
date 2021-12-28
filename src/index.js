ba59e7c619098a2ac3172f951c11b25ddeb8f1e4
Leandro Moreira <42127558+LeleMO@users.noreply.github.com>
2021-12-28 18:18:29 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title = '', url = '', techs = [] } = request.body
 
   const repository = {
     id: uuid(),
@@ -23,11 +23,12 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
+  const { title = '', url = '', techs = [] } = request.body;
   const updatedRepository = request.body;
 
   repositoryIndex = repositories.findindex(repository => repository.id === id);
@@ -35,6 +36,12 @@ app.put("/repositories/:id", (request, response) => {
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+   const updatedRepository = {
+    ...repository,
+    title,
+    url,
+    techs
+  };
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
@@ -44,31 +51,34 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  repositories.splice(repositoryIndex, 1);
-
-  return response.status(204).send();
+ const { id } = request.params;
+  const repository = repositories.find(repository => repository.id === id);
+  if (!repository)
+    return response.status(400).json({
+      message: 'repository not found'
+    });
+  const filteredRepositories = repositories.filter(repository => repository.id !== id);
+  repositories = filteredRepositories;
+  return response.status(204).json({
+    message: 'repository has been successfully deleted'
+  });
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
-
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  const repositoryToAddLike = repositories.find(repository => repository.id === id);
+  if (!repositoryToAddLike)
+    return response.status(400).json({
+      message: 'repository not found'
+    });
+  repositoryToAddLike.likes++;
+  repositories = repositories.map(repository => {
+    if (repository.id === id)
+      return repositoryToAddLike;
+    else
+      return repository;
+  });
+  return response.status(200).json(repositoryToAddLike);
 });
 
 module.exports = app;
